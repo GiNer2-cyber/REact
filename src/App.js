1bbcb3d7a20c14876fca204b94dcb17dee4b7277
GiNer2-cyber <139228391+GiNer2-cyber@users.noreply.github.com>
2023-11-01 02:29:54 +0100

    homework2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import {Simpsons} from "./components/SimpsonContainer/Simpsons";
-import {Characters} from "./components/CharactersContainer/Characters";
+import {Posts} from "./components/PostsContainer/Posts/Posts";
+import {Launches} from "./components/LaunchesContainer/Launches/Launches";
 
 const App = () => {
     return (
         <div>
-            {<Simpsons/>}
-            <Characters/>
+            {<Posts/>}
+            <Launches/>
         </div>
     );
 };
